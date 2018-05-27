4956b0b39db25561439aa83bb97dfc784d9ceccc
Laurent Bassin <laurent@bassin.info>
2018-05-27 20:34:32 +0200

    Add default nginx config

--- images/nginx/entrypoint.sh
+++ images/nginx/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 set -e
-CONFIG_START='server {'
 
 if [ ! -z "$MAX_UPLOAD_SIZE" ]
 then
     echo "Set max upload size";
-    sed -i "s/^${CONFIG_START}$/${CONFIG_START}\nclient_max_body_size ${MAX_UPLOAD_SIZE};\n/g" /etc/nginx/conf.d/default.conf
+    sed -i "s/^#MAX_UPLOAD_SIZE#$/client_max_body_size ${MAX_UPLOAD_SIZE};/g" /etc/nginx/conf.d/default.conf
 fi
 
 exec /usr/sbin/nginx -g 'daemon off;'
