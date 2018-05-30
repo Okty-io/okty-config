555da46944e12012f070363cc902faca0f0cdfb2
Laurent Bassin <laurent@bassin.info>
2018-05-31 00:06:20 +0200

    Fix nginx max upload size

--- images/nginx/entrypoint.sh
+++ images/nginx/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 if [ ! -z "$MAX_UPLOAD_SIZE" ]
 then
     echo "Set max upload size";
-    sed -i "s/^#MAX_UPLOAD_SIZE#$/client_max_body_size ${MAX_UPLOAD_SIZE};/g" /etc/nginx/conf.d/default.conf
+    sed -i "s/#MAX_UPLOAD_SIZE#/client_max_body_size ${MAX_UPLOAD_SIZE};/g" /etc/nginx/conf.d/default.conf
 fi
 
 exec /usr/sbin/nginx -g 'daemon off;'
