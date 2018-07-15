8f23ab0da1969b2cce8d3b97fe78dd1d4c9608fa
Laurent Bassin <laurent@bassin.info>
2018-07-15 14:09:06 +0200

    Clean deleted file

---
+++ deploy/clean_storage.js
@@ -0,0 +1,42 @@
+global.XMLHttpRequest = require("xhr2");
+const fs = require('fs');
+const Storage = require('@google-cloud/storage');
+
+const config = {
+    apiKey: process.env.AWS_ACCESS_KEY_ID,
+    storageBucket: 'okty-7e60c.appspot.com',
+    projectId: 'okty-7e60c'
+};
+
+const storage = new Storage(config);
+
+const path = 'containers/';
+
+getLocalFiles(path);
+getRemoteFiles(path);
+
+function getLocalFiles(folder) {
+    const files = [];
+
+    fs.readdirSync(folder).forEach((filename) => {
+        const path = folder + filename;
+
+        if (!fs.lstatSync(path).isDirectory()) {
+            files.push(path);
+        }
+    });
+
+    return files;
+}
+
+function getRemoteFiles(folder) {
+    const files = [];
+
+    storage.bucket(config.storageBucket).getFiles({prefix: folder}, (error, files) => {
+        console.log(error);
+    });
+
+    console.log(files);
+
+    return files;
+}
