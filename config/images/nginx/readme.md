989312171b2a68da65994a92b9145bf23fc4eb0c
Laurent Bassin <laurent@bassin.info>
2018-10-27 22:47:15 +0200

    Add nginx container

--- config/images/nginx/readme.md
+++
@@ -1,4 +0,0 @@
-``` bash
-docker build -t okty/nginx .
-docker push okty/nginx
-```
--- containers/nginx/resolvers.php
+++ containers/nginx/resolvers.php
@@ -2,10 +2,30 @@
 
 function max_upload_size($value)
 {
-    return "client_max_body_size $value;";
+    return !empty($value) ? "client_max_body_size $value;" : '';
+}
+
+function root_folder($value)
+{
+    if (empty($value)) {
+        return '/usr/share/nginx/html';
+    }
+
+    return '/usr/share/nginx/html/' . $value;
 }
 
 function php_container_link($value)
 {
-    return $value;
+    if (empty($value)) {
+        return '';
+    }
+
+    return <<<EOT
+    location ~ \.php$ {
+        fastcgi_pass   ${value}:9000;
+        fastcgi_index  index.php;
+        fastcgi_param  SCRIPT_FILENAME  \$document_root\$fastcgi_script_name;
+        include        fastcgi_params;
+    };
+EOT;
 }
