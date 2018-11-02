749c452139a7341a2b3cf7dd913f99acbf630992
Laurent Bassin <laurent@bassin.info>
2018-11-02 14:21:23 +0100

    Add build and upload scripts

---
+++ deploy/upload.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd build
+for file in *; do
+    echo "Uploading ${file} ..."
+
+    container="${file%.*}"
+
+    aws lambda get-function --function-name ${container} > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        aws lambda update-function-code \
+            --function-name ${container} \
+            --zip-file fileb://${file}
+    else
+        aws lambda create-function \
+            --function-name ${container} \
+            --zip-file fileb://${file} \
+            --runtime nodejs8.10 \
+            --handler handler.handle \
+            --role arn:aws:iam::593664064329:role/service-role/ResolverRole
+    fi
+done
