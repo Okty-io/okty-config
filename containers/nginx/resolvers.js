de3b747e03d8b009073b9c50713ee15dc0f10f3d
Laurent Bassin <laurent@bassin.info>
2018-11-09 19:23:29 +0100

    Fix php link

--- containers/nginx/resolvers.js
+++ containers/nginx/resolvers.js
@@ -17,12 +17,12 @@ module.exports = {
         }
 
         return `
-            location ~ \\.php$ {
+            location ~ \.php$ {
                 fastcgi_pass   ${value}:9000;
                 fastcgi_index  index.php;
-                fastcgi_param  SCRIPT_FILENAME  \\$document_root\\$fastcgi_script_name;
+                fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
                 include        fastcgi_params;
-            };
+            }
         `;
     }
 };
